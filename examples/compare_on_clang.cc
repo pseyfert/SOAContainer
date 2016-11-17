ccbe8c9ec00b74303d675304427fbfc026fbe32e
Manuel Tobias Schiller <mala@hinterbergen.de>
2016-11-17 11:20:06 +0100

    make clang spit out fewer warnings

--- examples/compare_on_clang.cc
+++ examples/compare_on_clang.cc
@@ -14,12 +14,12 @@ int main() {
     const NullSkin<DressedTuple<std::tuple<const double &, const int &, const int &>, SOAContainer<std::vector, NullSkin, double, int, int> > > val1 = c.front();
     const NullSkin<DressedTuple<std::tuple<const double &, const int &, const int &>, SOAContainer<std::vector, NullSkin, double, int, int> > > val2 = c.back();
     
-    val1 == val2;
+    (void) (val1 == val2);
     
     const SOAObjectProxy<SOAContainer<std::vector, NullSkin, double, int, int> >  val3 = c.front();
     const SOAObjectProxy<SOAContainer<std::vector, NullSkin, double, int, int> >  val4 = c.back();
     
-    val4 == val3;
+    (void) (val4 == val3);
     
     
     return 0;
