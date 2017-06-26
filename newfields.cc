39af7d6a325610a66844621b72aeed8d27a2e55c
Manuel Tobias Schiller <mala@hinterbergen.de>
2017-06-26 11:53:37 +0200

    more work on new fields

--- newfields.cc
+++ newfields.cc
@@ -5,11 +5,12 @@
 #include "include/SOAContainer.h"
 #include "include/PrintableNullSkin.h"
 
+/* library code  */
 template <typename BASE, typename T>
 struct FieldBase {
     using wrap_tag = struct {};
     static_assert(!SOATypelist::is_wrapped<T>::value,
-	"It is an error to nest FieldBase<BASE, FieldBase<T> >!");
+        "It is an error to nest FieldBase<BASE, FieldBase<T> >!");
     using type = T;
     type& operator()()
     { return static_cast<BASE&>(*this).template get<FieldBase<BASE, type> >(); }
@@ -17,9 +18,9 @@ struct FieldBase {
     { return static_cast<const BASE&>(*this).template get<FieldBase<BASE, type> >(); }
 };
 
-
 template <typename BASE, template <typename> class... FIELDS>
-struct SkinBase : BASE //, FIELDS<BASE>...
+struct SkinBase : FIELDS<BASE>..., BASE
+                  // order above important for empty base class optimisation
 {
     using skin_tag = struct {};
     using fields_typelist = SOATypelist::typelist<FIELDS<BASE>... >;
@@ -27,7 +28,13 @@ struct SkinBase : BASE //, FIELDS<BASE>...
     using BASE::operator=;
 };
 
-template <typename BASE>
+template <typename T, template <typename> class... FIELDS>
+auto _detect_skin_base(const SkinBase<T, FIELDS...>&) -> SkinBase<T, FIELDS...>;
+template <typename T>
+using detect_skin_base = decltype(_detect_skin_base(std::declval<const T&>()));
+
+/* user code */
+template <typename BASE> // this may be packaged up as a macro...
 struct f_x : FieldBase<BASE, float>
 {
     float& x() { return this->operator(); }
@@ -46,30 +53,31 @@ struct f_n : FieldBase<BASE, int>
     int const& n() const { return this->operator(); }
 };
 
-template <typename T, template <typename> class... FIELDS>
-SkinBase<T, FIELDS...> _detect_skin_base(const SkinBase<T, FIELDS...>&);
-template <typename T>
-using detect_skin_base = decltype(_detect_skin_base(std::declval<const T&>()));
-
 template <typename BASE>
 struct Skin : SkinBase<PrintableNullSkin<BASE>, f_x, f_y, f_n>
 {
-    using B = detect_skin_base<Skin>;
+    using B = detect_skin_base<Skin<BASE> >;
     using B::B;
     using B::operator=;
 };
 
+/* small test driver... */
 int main()
 {
+    SOAContainer<std::vector, NullSkin, float, float, int> s;
     std::tuple<float, float, int> t(3.14, 2.79, 42);
+    std::cout << "naive proxy has size " <<
+        sizeof(s.begin()) << std::endl;
     SOAContainer<std::vector, Skin> c;
+    std::cout << "proxy has size " <<
+        sizeof(c.begin()) << std::endl;
     c.push_back(t);
     std::cout << c[0] << std::endl;
     struct Foo {};
     SkinBase<Foo, f_x, f_y, f_n> sb;
     std::cout << "SkinBase<Foo, f_x, f_y, f_n> has size " <<
-	sizeof(sb) << std::endl;
+        sizeof(sb) << std::endl;
     return 0;
 }
 
-
+// vim: sw=4:tw=78:ft=cpp:et
