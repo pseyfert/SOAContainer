176ad9fc599949de781abcb7d299bce7240454fd
Manuel Tobias Schiller <mala@hinterbergen.de>
2017-06-27 18:34:50 +0200

    revert newfields.cc to what is almost Florian Lemaitre's proposal
    Now all the bits work as expected.
    
    Wishlist:
    - integration into the main SOA framework
    - a SOAView aggregation convenience function that allows you to join up
      skins and fields, and returns a new view...
    - write something that will allow you to clone all or parts of a skin
      into a new skin, complete with field typelist with "copied types", so
      that joining up the same fields from two unrelated containers can work
      without clash (e.g. think of a StateVectorSkin that is part of a T
      track and part of a Velo track - if you create a view of these
      (assuming they match up), you'd like to be able to address both...)

--- newfields.cc
+++ newfields.cc
@@ -6,11 +6,14 @@
 #include "include/PrintableNullSkin.h"
 
 /* library code  */
+
 /** @brief class from which a field tag derives
  *
- * @tparam T    type of the field
+ * @tparam T            type of the field
+ * @tparam FIELD        field (child) type to use
+ * @tparam SKIN         skin to which we're bound
  */
-template <typename T>
+template <typename T, template <typename> class FIELD, typename SKIN>
 class FieldBase {
     public:
         /// the field tag itself
@@ -18,25 +21,24 @@ class FieldBase {
         /// tell SOAContainer/View that we wrap a type
         using wrap_tag = struct {};
         static_assert(!SOATypelist::is_wrapped<T>::value,
-                "It is an error to nest FieldBase<BASE, FieldBase<T> >!");
+                "Error: Do no nest FieldBase<FieldBase<T>, FIELD, SKIN>!");
         /// type we wrap
         using type = T;
-        /// support rebinding to the underlying base (tuple/...)
-        template <typename BASE, typename PARENT>
-        class rebind : public PARENT {
-            protected:
-                /// read-write access to field
-                type& _get()
-                { return static_cast<BASE&>(*this).template get<self_type>(); }
-                /// read-only access to field
-                type const& _get() const
-                { return static_cast<const BASE&>(*this).template get<self_type>(); }
-        };
+        /// support rebinding to a different skin type
+        template <typename NEWSKIN> using rebind = FIELD<NEWSKIN>;
     protected:
-        /// stub: read-write access to field (never called)
-        type& _get();
-        /// stub: read-only access to field (never called)
-        type const& _get() const;
+        /// retrieve reference to the field
+        type& _get()
+        {
+            return reinterpret_cast<SKIN&>(*this).template
+                get<FIELD<void> >();
+        }
+        /// retrieve const reference to the field
+        const type& _get() const
+        {
+            return reinterpret_cast<const SKIN&>(*this).template
+                get<FIELD<void> >();
+        }
 };
 
 /** @brief base class of all skins
@@ -44,49 +46,66 @@ class FieldBase {
  * @tparam BASE         underlying tuple/whatever
  * @tparam FIELDS       fields to which grant access
  */
-template <typename BASE, typename... FIELDS>
-struct SkinBase : FIELDS::template rebind<BASE, FIELDS>..., BASE
+template <typename BASE, template <typename> class... FIELDS>
+struct SkinBase : FIELDS<BASE>..., BASE
                   // order above important for empty base class optimisation
 {
     // make sure that no user puts data in a field...
-    static_assert(SOAUtils::ALL<std::is_empty,
-            typename FIELDS::template rebind<BASE, FIELDS>...>::value,
+    static_assert(SOAUtils::ALL<std::is_empty, FIELDS<BASE>...>::value,
             "Fields may not contain data or virtual methods!");
     /// inform the framework that we're a skin
     using skin_tag = struct {};
     /// we're the base class underlying classes that inherit from us
     using base_type = SkinBase;
     /// typelist listing all fields
-    using fields_typelist = SOATypelist::typelist<FIELDS...>;
+    using fields_typelist = SOATypelist::typelist<FIELDS<void>...>;
     /// forward to *underlying constructors operators
     using BASE::BASE;
     /// forward to *underlying assignment operators
     using BASE::operator=;
+    // FIXME: extract fields -> new skin class, new field tags
+    // FIXME: make skin subobject -> new skin class, new field tags
 };
 
 /// little helper to detect the base type of a skin (saves typing)
 template <typename T>
 using detect_skin_base = typename T::base_type;
 
+/// defined a field with name name and type type, with custom accessors
+#define SOAFIELD_CUSTOM(name, type) \
+    template <typename SKIN = void> \
+    struct name : FieldBase<type, name, SKIN>
+/// define a field with name name, accessors named accessorname, of type type
+#define SOAFIELD(name, accessorname, type) \
+    SOAFIELD_CUSTOM(name, type) { \
+        type& accessorname() { return this->_get(); } \
+        const type& accessorname() const { return this->_get(); } \
+    }
 /* user code */
-struct f_x : FieldBase<float> // this may be packaged up as a macro...
-{
-    float& x() { return this->_get(); }
-    float const& x() const { return this->_get(); }
-};
-struct f_y : FieldBase<float>
-{
-    float& y() { return this->_get(); }
-    float const& y() const { return this->_get(); }
-};
-struct f_n : FieldBase<int>
-{
-    int& n() { return this->_get(); }
-    int const& n() const { return this->_get(); }
+SOAFIELD(f_x, x, float);
+SOAFIELD(f_y, y, float);
+SOAFIELD_CUSTOM(f_flags, int) {
+    enum Flag { Used = 0x1, Dead = 0x2 };
+    int& flags() { return this->_get(); }
+    const int& flags() const { return this->_get(); }
+    bool isUsed() const { return flags() & Used; }
+    bool isDead() const { return flags() & Dead; }
+    bool setUsed(bool newState = true)
+    {
+        int retVal = flags();
+        flags() = (retVal & ~Used) | (-newState & Used);
+        return retVal & Used;
+    }
+    bool setDead(bool newState = true)
+    {
+        int retVal = flags();
+        flags() = (retVal & ~Dead) | (-newState & Dead);
+        return retVal & Dead;
+    }
 };
 
 template <typename BASE>
-struct Skin : SkinBase<PrintableNullSkin<BASE>, f_x, f_y, f_n>
+struct Skin : SkinBase<PrintableNullSkin<BASE>, f_x, f_y, f_flags>
 {
     using B = detect_skin_base<Skin>;
     using B::B;
@@ -97,18 +116,23 @@ struct Skin : SkinBase<PrintableNullSkin<BASE>, f_x, f_y, f_n>
 int main()
 {
     SOAContainer<std::vector, NullSkin, float, float, int> s;
-    std::tuple<float, float, int> t(3.14, 2.79, 42);
-    std::cout << "naive proxy has size " <<
-        sizeof(s.begin()) << std::endl;
     SOAContainer<std::vector, Skin> c;
-    std::cout << "proxy has size " <<
-        sizeof(c.begin()) << std::endl;
-    c.push_back(t);
+    static_assert(sizeof(s.front()) == sizeof(c.front()),
+            "Fancy field and old-style field peoxies need to have same size.");
+    c.push_back(std::make_tuple(3.14f, 2.79f, 42));
+    // print c[0]
+    std::cout << c[0] << std::endl;
+    // work a bit with c[0]
+    std::cout << c[0].x() << " " << c[0].y() << " " << c[0].isUsed() << std::endl;
+    c.front().setDead(false);
+    // this won't work - constness
+    // const_cast<const decltype(c)&>(c).front().setUsed();
+    //
+    // ... and print it again
     std::cout << c[0] << std::endl;
     struct Foo { int i; };
-    SkinBase<Foo, f_x, f_y, f_n> sb;
-    std::cout << "SkinBase<Foo, f_x, f_y, f_n> has size " <<
-        sizeof(sb) << std::endl;
+    SkinBase<Foo, f_x, f_y, f_flags> sb;
+    static_assert(sizeof(int) == sizeof(sb), "skin size behaviour is all wrong.");
     return 0;
 }
 
