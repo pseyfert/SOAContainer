3a0b9c4e6defed48c657be834673a5fcb035655a
Manuel Tobias Schiller <mala@hinterbergen.de>
2017-10-03 12:38:37 +0100

    more work towards convenient fields
    I think I've seen more of Florian's deep wisdom: The field should be a
    class, not a template. The "rebind" has moved into a subclass of the
    field called and accessor, and the SkinBase and skin creation macros
    have been updated accordingly. Added a couple of convenience macros so
    users don't have to type quite so much...

--- newfields.cc
+++ newfields.cc
@@ -11,34 +11,33 @@
  *
  * @tparam T            type of the field
  * @tparam FIELD        field (child) type to use
- * @tparam SKIN         skin to which we're bound
  */
-template <typename T, template <typename> class FIELD, typename SKIN>
+template <typename T, class FIELD>
 class FieldBase {
     public:
         /// the field tag itself
-        using self_type = FieldBase;
+        using self_type = FIELD;
         /// tell SOAContainer/View that we wrap a type
         using wrap_tag = struct {};
+        // make sure that the user doesn't do strange things...
         static_assert(!SOATypelist::is_wrapped<T>::value,
-                "Error: Do no nest FieldBase<FieldBase<T>, FIELD, SKIN>!");
+                "Error: Do no nest FieldBase<FieldBase<T>, FIELD>!");
         /// type we wrap
         using type = T;
-        /// support rebinding to a different skin type
-        template <typename NEWSKIN> using rebind = FIELD<NEWSKIN>;
     protected:
-        /// retrieve reference to the field
-        type& _get()
-        {
-            return reinterpret_cast<SKIN&>(*this).template
-                get<FIELD<void> >();
-        }
-        /// retrieve const reference to the field
-        const type& _get() const
-        {
-            return reinterpret_cast<const SKIN&>(*this).template
-                get<FIELD<void> >();
-        }
+        /** @brief low-level struct for as base class for accessors
+         *
+         * @tparam SKIN skin which this accessor will use
+         */
+        template <class SKIN>
+        struct AccessorBase {
+            /// retrieve reference to the field
+            type& _get()
+            { return reinterpret_cast<SKIN&>(*this).template get<FIELD>(); }
+            /// retrieve const reference to the field
+            const type& _get() const
+            { return reinterpret_cast<const SKIN&>(*this).template get<FIELD>(); }
+        };
 };
 
 /** @brief base class of all skins
@@ -46,19 +45,22 @@ class FieldBase {
  * @tparam BASE         underlying tuple/whatever
  * @tparam FIELDS       fields to which grant access
  */
-template <typename BASE, template <typename> class... FIELDS>
-struct SkinBase : FIELDS<BASE>..., BASE
+template <typename BASE, class... FIELDS>
+struct SkinBase : FIELDS::template accessors<BASE>..., BASE
                   // order above important for empty base class optimisation
 {
     // make sure that no user puts data in a field...
-    static_assert(SOAUtils::ALL<std::is_empty, FIELDS<BASE>...>::value,
+    static_assert(SOAUtils::ALL<std::is_empty, FIELDS...>::value,
             "Fields may not contain data or virtual methods!");
+    static_assert(SOAUtils::ALL<std::is_empty,
+            typename FIELDS::template accessors<BASE>...>::value,
+            "Field accessors may not contain data or virtual methods!");
     /// inform the framework that we're a skin
     using skin_tag = struct {};
     /// we're the base class underlying classes that inherit from us
     using base_type = SkinBase;
     /// typelist listing all fields
-    using fields_typelist = SOATypelist::typelist<FIELDS<void>...>;
+    using fields_typelist = SOATypelist::typelist<FIELDS...>;
     /// forward to *underlying constructors operators
     using BASE::BASE;
     /// forward to *underlying assignment operators
@@ -72,19 +74,27 @@ template <typename T>
 using detect_skin_base = typename T::base_type;
 
 /// defined a field with name name and type type, with custom accessors
-#define SOAFIELD_CUSTOM(name, type) \
-    template <typename SKIN = void> \
-    struct name : FieldBase<type, name, SKIN>
+#define SOAFIELD_CUSTOM(name, type, ... /* body */) \
+    struct name : FieldBase<type, name> { \
+        template <class SKIN> struct accessors : AccessorBase<SKIN> \
+        { __VA_ARGS__ }; }
 /// define a field with name name, accessors named accessorname, of type type
 #define SOAFIELD(name, accessorname, type) \
-    SOAFIELD_CUSTOM(name, type) { \
+    SOAFIELD_CUSTOM(name, type, \
         type& accessorname() { return this->_get(); } \
         const type& accessorname() const { return this->_get(); } \
-    }
+    )
+/// define a skin
+#define SOASKIN_DECL(name, ... /* fields */) \
+template <class BASE> struct name : SkinBase<PrintableNullSkin<BASE>, __VA_ARGS__>
+#define SOASKIN_INHERIT_CONSTRUCTORS_ASSIGNMENTS(name) \
+    using B = detect_skin_base<name>; \
+    using B::B; using B::operator=
+
 /* user code */
 SOAFIELD(f_x, x, float);
 SOAFIELD(f_y, y, float);
-SOAFIELD_CUSTOM(f_flags, int) {
+SOAFIELD_CUSTOM(f_flags, int,
     enum Flag { Used = 0x1, Dead = 0x2 };
     int& flags() { return this->_get(); }
     const int& flags() const { return this->_get(); }
@@ -102,16 +112,20 @@ SOAFIELD_CUSTOM(f_flags, int) {
         flags() = (retVal & ~Dead) | (-newState & Dead);
         return retVal & Dead;
     }
-};
+    void printflags() { std::printf("flags: %08x\n", flags()); }
+);
 
-template <typename BASE>
-struct Skin : SkinBase<PrintableNullSkin<BASE>, f_x, f_y, f_flags>
-{
-    using B = detect_skin_base<Skin>;
-    using B::B;
-    using B::operator=;
+SOASKIN_DECL(Skin, f_x, f_y, f_flags) {
+    SOASKIN_INHERIT_CONSTRUCTORS_ASSIGNMENTS(Skin);
     // special constructors go here...
     // special accessors go here...
+    void setDeadIfTooFarOut()
+    {
+        // inside the skin, this->accessor() is required to make C++ find the
+        // routine, users of the skin can just call accessor()
+        auto x = this->x(), y = this->y();
+        if ((x * x + y + y) > 1.f) this->setDead();
+    }
 };
 
 /* small test driver... */
