5492b2f32debd2276cbc20b12f3b83d45d8beef3
Manuel Tobias Schiller <mala@hinterbergen.de>
2018-03-06 21:06:43 +0100

    add test for std::stable_partition

--- tests/SOAContainerTest.cc
+++ tests/SOAContainerTest.cc
@@ -785,4 +785,32 @@ TEST(SOAView, ZipViews) {
     }
 }
 
+namespace PartitionTestDesc {
+    SOAFIELD_TRIVIAL(n, n, unsigned);
+    SOASKIN_TRIVIAL(Skin, n);
+}
+
+TEST(SOAContainer, Partition) {
+    SOA::Container<std::vector, PartitionTestDesc::Skin> c;
+    c.reserve(32);
+    for (unsigned i = 0; i < 32; ++i) c.emplace_back(i);
+    EXPECT_EQ(c.size(), 32u);
+    auto isEven = [] (decltype(c)::value_const_reference a) noexcept { return !(a.n() & 1); }; 
+    const auto partitioned0 = std::is_partitioned(c.begin(), c.end(), isEven);
+    EXPECT_EQ(partitioned0, false);
+    const auto it1 = std::partition(c.begin(), c.end(), isEven);
+    const auto partitioned1 = std::is_partitioned(c.begin(), c.end(), isEven);
+    EXPECT_EQ(partitioned1, true);
+    EXPECT_EQ(it1, c.begin() + 16);
+    c.clear();
+    for (unsigned i = 0; i < 32; ++i) c.emplace_back(i);
+    EXPECT_EQ(c.size(), 32u);
+    const auto partitioned2 = std::is_partitioned(c.begin(), c.end(), isEven);
+    EXPECT_EQ(partitioned2, false);
+    const auto it3 = std::stable_partition(c.begin(), c.end(), isEven);
+    const auto partitioned3 = std::is_partitioned(c.begin(), c.end(), isEven);
+    EXPECT_EQ(partitioned3, true);
+    EXPECT_EQ(it3, c.begin() + 16);
+}
+
 // vim: sw=4:tw=78:ft=cpp:et
