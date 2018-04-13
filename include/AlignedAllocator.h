d87377272fcda41e15750474b3d778fb6347b2a2
Manuel Tobias Schiller <mala@hinterbergen.de>
2018-04-13 12:39:06 +0100

    remove -Weffc++ again, modernise and clean up AlignedAllocator
    gcc 4.8 barks at things inheriting from std::false_type with -Weffc++,
    that's no good, so we remove it again
    
    AlignedAllocator:
    - no longer pollute global namespace
    - remove some typedefs (unfortunately, gcc versions before 5.x still
      need a lot of the ugly bits from the past (instead of taking it from
      allocator_traits)

--- include/AlignedAllocator.h
+++ include/AlignedAllocator.h
@@ -10,78 +10,81 @@
 #ifndef ALIGNEDALLOCATOR_H
 #define ALIGNEDALLOCATOR_H
 
-/** @brief aligned allocator
- *
- * @author Manuel Schiller <Manuel.Schiller@glasgow.ac.uk>
- * @date 2017-03-28
- *
- * This class allows for allocation of aligned memory for classes like
- * std::vector. It is used this way:
- *
- * @code
- * #include <vector>
- * #include "AlignedAllocator.h"
- * template <typename T> using aligned_vector =
- *     std::vector<T, AlignedAllocator<T, 64> >;
- * aligned_vector<int> v;
- * v.push_back(42); // more operations
- * @endcode
- *
- * The class has the following template parameters:
- *
- * @tparam T            type for which to allocate memory
- * @tparam ALIGN        alignment in bytes
- *
- * ALIGN must be a power of two between 1 and 128 bytes. (Larger alignment
- * requirements are better satisfied with a more complex allocator.)
- */
-template <typename T, std::size_t ALIGN>
-class AlignedAllocator {
+namespace SOA {
+    /** @brief aligned allocator
+     *
+     * @author Manuel Schiller <Manuel.Schiller@glasgow.ac.uk>
+     * @date 2017-03-28
+     *
+     * This class allows for allocation of aligned memory for classes like
+     * std::vector. It is used this way:
+     *
+     * @code
+     * #include <vector>
+     * #include "AlignedAllocator.h"
+     * template <typename T> using aligned_vector =
+     *     std::vector<T, AlignedAllocator<T, 64> >;
+     * aligned_vector<int> v;
+     * v.push_back(42); // more operations
+     * @endcode
+     *
+     * The class has the following template parameters:
+     *
+     * @tparam T            type for which to allocate memory
+     * @tparam ALIGN        alignment in bytes
+     *
+     * ALIGN must be a power of two between 1 and 128 bytes. (Larger alignment
+     * requirements are better satisfied with a more complex allocator.)
+     */
+    template <typename T, std::size_t ALIGN>
+    class AlignedAllocator {
     private:
         // a few sanity checks...
         static_assert(ALIGN > 0, "ALIGN must be positive.");
         static_assert(ALIGN <= 128, "ALIGN must be 128 or smaller.");
         static_assert(0 == (ALIGN & (ALIGN - 1)),
-                "ALIGN must be a power of 2.");
-        static_assert(ALIGN >= ((sizeof(T) < 16) ? sizeof(T) : 16),
-                "ALIGN not suitable for type T");
+                      "ALIGN must be a power of 2.");
+        static_assert(0 == (ALIGN % alignof(T)),
+                      "ALIGN not suitable for type T");
 
         std::allocator<char> alloc; ///< underlying allocator
 
     public:
-        /// constructor
-        AlignedAllocator() = default;
-        /// copy constructor
-        AlignedAllocator(const AlignedAllocator<T, ALIGN>&) = default;
-        /// rebinding constructor
-        template <typename U>
-        AlignedAllocator(const AlignedAllocator<U, ALIGN>&) {}
-
-        /// type being allocated
         using value_type = T;
-        /// type of pointer
         using pointer = T*;
-        /// type of const pointer
         using const_pointer = const T*;
-        /// type of reference
         using reference = T&;
-        /// type of const_reference
         using const_reference = const T&;
-        /// type used for sizes
         using size_type = std::size_t;
-        /// type used for (pointer) differences
         using difference_type = std::ptrdiff_t;
-        /// whether to propagate allocator on move assignment of containers
+        using propagate_on_container_copy_assignment = std::true_type;
         using propagate_on_container_move_assignment = std::true_type;
-        /// allocator type for a different underlying type -- U instead of T
-        template <typename U>
-        struct rebind { using other = AlignedAllocator<U, ALIGN>; };
-        // C++17
-        // using is_always_equal = typename std::allocator<T>::is_always_equal;
+        using propagate_on_container_swap = std::true_type;
+        using is_always_equal = std::true_type;
+        constexpr AlignedAllocator<T, ALIGN>
+        select_on_container_copy_construction() const noexcept
+        {
+            return {};
+        }
 
-        /// allocate (aligned) space for n objects of type T
-        pointer __attribute__((aligned(ALIGN))) allocate(
-                size_type n, const void* hint = nullptr)
+        AlignedAllocator() = default;
+        AlignedAllocator(const AlignedAllocator<T, ALIGN>&) = default;
+        AlignedAllocator(AlignedAllocator<T, ALIGN>&&) = default;
+        AlignedAllocator<T, ALIGN>&
+        operator=(const AlignedAllocator<T, ALIGN>&) = default;
+        AlignedAllocator<T, ALIGN>&
+        operator=(AlignedAllocator<T, ALIGN>&&) = default;
+        template <typename U>
+        constexpr AlignedAllocator(const AlignedAllocator<U, ALIGN>&)
+        {}
+
+
+        template <typename U>
+        struct rebind {
+            using other = AlignedAllocator<U, ALIGN>;
+        };
+
+        pointer allocate(size_type n, const void* hint = nullptr)
         {
             char* p = alloc.allocate(n * sizeof(T) + ALIGN, hint);
             p += ALIGN;
@@ -91,7 +94,6 @@ class AlignedAllocator {
             return (T*) p;
         }
 
-        /// deallocate allocated space
         void deallocate(pointer p, std::size_t n)
         {
             auto adj = *(((unsigned char*) p) - 1);
@@ -99,30 +101,50 @@ class AlignedAllocator {
             alloc.deallocate(q, n * sizeof(T) + ALIGN);
         }
 
-        /// maximum size representable by size_type
         constexpr size_type max_size() const noexcept
-        { return std::numeric_limits<size_type>::max(); }
+        {
+            return std::numeric_limits<size_type>::max() / sizeof(T);
+        }
+        constexpr bool operator==(const AlignedAllocator<T, ALIGN>&) const
+                noexcept
+        {
+            return true;
+        }
+        constexpr bool operator!=(const AlignedAllocator<T, ALIGN>&) const
+                noexcept
+        {
+            return false;
+        }
 
-        /// construct a T at memory location p
-        void construct(pointer p, const_reference val) { new(p) T(val); }
-        /// construct a U at memory location p
+#if defined(__GNUC__) && !defined(__clang__) &&                              \
+        !defined(__INTEL_COMPILER) && __GNUC__ < 5
+        void construct(pointer p, const_reference val) const
+        {
+            new (p) T(val);
+        }
         template <typename U, typename... ARGS>
-        void construct(U* p, ARGS&&... args) { new(p) U(std::forward<ARGS>(args)...); }
-        /// destroy a T at memory location p
-        void destroy(pointer p) { p->~T(); }
-        /// destroy a U at memory location p
+        void construct(U* p, ARGS&&... args) const
+        {
+            new (p) U(std::forward<ARGS>(args)...);
+        }
+        void destroy(pointer p) const { p->~T(); }
         template <typename U>
-        void destroy(U* p) { p->~U(); }
+        void destroy(U* p) const
+        {
+            p->~U();
+        }
+        constexpr pointer address(reference r) const noexcept { return &r; }
+        constexpr const_pointer address(const_reference r) const noexcept
+        {
+            return &r;
+        }
+#endif
+    };
 
-        /// return the address of what reference references
-        pointer address(reference r) const noexcept { return &r; }
-        /// return the address of what const reference references
-        const_pointer address(const_reference r) const noexcept { return &r; }
-};
-
-/// convenience typedef for 64 byte alignment
-template <typename T>
-using CacheLineAlignedAllocator = AlignedAllocator<T, 64>;
+    /// convenience typedef for 64 byte alignment
+    template <typename T>
+    using CacheLineAlignedAllocator = AlignedAllocator<T, 64>;
+} // namespace SOA
 
 #endif // ALIGNEDALLOCATOR_H
 
